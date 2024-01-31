c1f8eee63557021efadb935e152770b6f43a4720
Diego Martinez <d.martinez@sisproconsultores.com>
2024-01-31 11:28:52 +0100

    Change text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,25 @@
 import styles from "./page.module.css";
 import config from "../config/main.json";
 import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Welcome to APIM Boilerplate from FrontEnd folder</h1>
-      <Box>Hello</Box>
-      <a
-        href={config.app_host_name}
-        // target="_blank"
-        rel="noopener noreferrer"
-      >
-        <h2>Home</h2>
-      </a>
+      <Box m={5}>
+        <Typography variant="h1">Hi common components TEAM</Typography>
+        <Typography variant="body1">
+          This is a presentation o f how to use ChatGPT
+        </Typography>
+        <Box>Hello</Box>
+        {/* <a
+          href={config.app_host_name}
+          // target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>Home</h2>
+        </a> */}
+      </Box>
     </main>
   );
 }
